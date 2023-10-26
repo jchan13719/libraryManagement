deefed1c9433ff3f470b53998bff0abf95e41a02
Justin Chan <justin.chan.3719@gmail.com>
2023-10-25 22:19:27 -0400

    Add init

---
+++ src/test/java/com/tcs/libraryManagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.libraryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
